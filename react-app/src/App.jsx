8928d865e8f5929da3f7b73e56c7f45314bd3c64
ShadyJoseph <shadyjosephabadeer2003@gmail.com>
2024-02-13 17:44:09 +0200

    loadingUpdate

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Quiz from './Components/Quiz/Quiz'
 const apiUrl = 'https://644982a3e7eb3378ca4ba471.mockapi.io/questions';
 function App() {
+  const [loading, setLoading] = useState(true);
   const [questions,setQuestions]=useState([])
   useEffect(()=>{
     fetchData();
@@ -14,11 +15,17 @@ const fetchData=async()=>  {
     setQuestions(questionResponse)
     if (!response.ok) {
       throw new Error(`Failed to fetch data. Status: ${response.status}`);
+      setLoading(false);
     }
   } catch (error) {
     console.error('Error fetching data:', error.message);
     throw error;
+  }finally{
+    setLoading(false);
   }
+}
+if (loading) {
+  return <h2>Loading...</h2>;
 }
 
   return (
